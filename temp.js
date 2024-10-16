3ec2f2ba69355c209e611d46287c55b9f53e9015
Cewerty <88933231+Cewerty@users.noreply.github.com>
2024-10-17 00:08:36 +0500

    Renamed couple of files, created a html page for temperature converter

--- temp.js
+++ temp.js
@@ -1,7 +1,13 @@
-let input = 23;
-let regimes = 'faringeit';
+'use strict';
 
-let testing_object = new Object({input: input, regime: regimes})
+let input = parseInt(prompt("Input value"));
+let regimes = prompt("What you waht to transform: celsius, faringeit, kelvin");
+let picked_translation = prompt("In what you waht to transform: celsius, faringeit, kelvin");
+
+regimes.toLowerCase();
+picked_translation.toLowerCase();
+
+let user_info = new Object({input: input, regime: regimes, translation: picked_translation})
 
 function get_Kel({input = 0, regime = 'celsius'}) {
     if (regime === 'celsius') {
@@ -39,4 +45,13 @@ function get_cel({input = 0, regime='kelvin'}) {
     }
 }
 
-console.log(get_Kel(testing_object));
+switch (user_info.translation) {
+    case 'kelvin':
+        alert(get_Kel(user_info));
+    case 'faringeit':
+        alert(get_Far(user_info));
+    case 'celsius':
+        alert(get_cel(user_info));
+    default:
+        alert("Error!");
+}
